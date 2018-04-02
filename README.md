a51607875ea62928ba3d39385803c65ae86112da
John Coleman <johncoleman_2@hotmail.com>
2018-04-01 23:04:05 -0500

    turn in

---
+++ README.md
@@ -0,0 +1,5 @@
+Issues:
+
+    1) I was unable to determine how to keep Spotify from returning songs that did not explicitly match the search criteria.  i.e. Searching for "The Sign" returned songs that didn't match that title.
+    2) Even when an exact macth was returned, it was not always the first returned. 
+    3) I held off on coding the do-what-it-says function until can figure out the Spotify issue.
--- liri.js
+++ liri.js
@@ -1,45 +1,147 @@
-// require("dotenv").config();
 
-var keys = require("./keys.js");
+require("dotenv").config();
 
-console.log(keys);
+const keyList = require('./keys');
+var fs = require("fs");
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var request = require('request');
+var inquirer = require("inquirer");
+var spotify = new spotify(keyList.spotify);
+var client = new twitter(keyList.twitter);
 
-// console.log(keys + "." + twitterKey);
 
-console.log("hello world");
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Please choose the command to execute:",
+            choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "command"
+        },
+    ])
+    .then(function (inquirerResponse) {
 
-// console.log("Twitter: " + keys.twitter);
-// console.log("Spotify: " + keys.spotify);
+        var command = inquirerResponse.command;
 
-// exports.twitter
+        switch (command) {
 
-for (var key in keys) {
+            case "my-tweets":
+                tweets();
+                break;
 
-    console.log("Key1 is: " + keys[key]);
+            case "spotify-this-song":
+                spotifyThisSong();
+                break;
 
-    // for (var key2 in key) {
-    //     // for (var key1 in key2) {
+            case "movie-this":
+                movieThis();
+                break;
 
-    //     // console.log(key2);
+            case "do-what-it-says":
+                doWhatItsays()
+                break;
 
-    //     console.log("key2: " + key + " " + key2);
+            default:
+                console.log("Invalid command " + command);
+                break;
+        };
+    })
 
-    //     console.log("Key2 is: " + key[key2]);
-    //     console.log(key + ": " + key2 + "[" + key2 + "]");
-    //     console.log("yep!!!");
 
-    // };
-
+function tweets() {
+    console.log(" ");
+    var params = { screen_name: 'jdckucoding2018' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            if (tweets.length <= 20) {
+                var numTweets = tweets.length;
+            } else {
+                var numTweets = 20;
+            };
+            for (i = 0; i < numTweets; i++) {
+                var tweetInfo = tweets[i];
+                console.log("text: " + tweetInfo.text);
+                console.log("created_at: " + tweetInfo.created_at);
+                console.log("----------------------------");
+            }
+        }
+    });
 };
 
-// console.log("-----------------");
-// console.log("spotify");
-// console.log("-----------------");
-// console.log(spotify);
-// console.log("-----------------");
+function spotifyThisSong() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Song name:",
+                name: "search"
+            },
+        ])
+        .then(function (inquirerResponse) {
+            if (inquirerResponse.search === '') {
+                search = "The Sign";
+            } else {
+                search = inquirerResponse.search;
+            }
+            // var command = inquirerResponse.command;
+            console.log(search);
+            spotify.search({ type: 'track', query: search}, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
 
-// console.log("-----------------");
-// console.log("twitter");
-// console.log("-----------------");
-// console.log(twitter);
-// console.log("-----------------");
+                spotifyArr = data.tracks;
+
+                console.log('---------------------------------------');
+                console.log(JSON.stringify(spotifyArr, null, 2));
+
+                //   console.log(JSON.parse(data.tracks.items.album)); 
+
+                // console.log(data.tracks.items.length);
+                // console.log(spotifyArr.artists);
+            });
+        });
+};
+
+function movieThis() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Movie name:",
+                name: "search"
+            },
+        ])
+        .then(function (inquirerResponse) {
+            if (inquirerResponse.search === '') {
+                search = "Mr. Nobody";
+            } else {
+                search = inquirerResponse.search;
+            }
+
+            var movieName = search;
+
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+            request(queryUrl, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    console.log(' ');
+                    console.log("Title                 : " + JSON.parse(body).Title);
+                    console.log("Release Year          : " + JSON.parse(body).Year);
+                    console.log("IMBD Rating           : " + JSON.parse(body).imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country Produced      : " + JSON.parse(body).Country);
+                    console.log("Language              : " + JSON.parse(body).Language);
+                    console.log("Plot                  : " + JSON.parse(body).Plot);
+                    console.log("Cast                  : " + JSON.parse(body).Actors);
+                    console.log(' ');
+                }
+            });
+        });
+};
+
+function doWhatItsays() {
+    console.log("yep 'do-what-it-says' is");
+};
